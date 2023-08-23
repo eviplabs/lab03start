fe32c65b2d21335785812728027cd50f88408a91
Kristof <kristof@aut.bme.hu>
2023-08-23 13:09:20 +0200

    initial commit

---
+++ LAB03_TextHandlingIoLab2/Solutions.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LAB03_TextHandlingIoLab2
+{
+    class Solutions
+    {
+        #region Examples and helper
+        internal bool MatchingExample()
+        {
+            var regex = new Regex(@"<.+>");
+            Match m = regex.Match(@"<valami>");
+            return m.Success;   // returns true
+        }
+
+        internal string[] CollectingMatchesExample()
+        {
+            // Returns "valami", "még valami" (anything except ">" between "<" and ">".
+            return Collect(@"<valami> és <még valami>", @"<[^>]+>").ToArray();
+        }
+
+        private IEnumerable<string> Collect(string text, string regex)
+        {
+            var re = new Regex(regex);
+            var matches = re.Matches(text);
+            foreach (Match match in matches)
+                yield return match.Captures[0].Value;
+        }
+        #endregion
+
+        #region Email
+        internal bool IsEmailAddress(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string[] CollectEmailAddresses(string s)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Phone numbers
+        internal bool IsPhoneNumber(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string[] CollectPhoneNumbers(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool IsHungarianMobilePhoneNumber(string v)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region MusicBox
+        internal bool IsInsideMusicBox(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string[] CollectWhatsInsideMusicBox(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal IEnumerable<char> HightlightNotesInMusicBox(string text)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region PlusCode
+        internal bool IsPlusCode(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool IsPlusCodeInBudapest(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string[] CollectFullPlusCodes(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Date
+        /// <summary>
+        /// Helper method to extract dates (as strings) from a string.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        internal string[] CollectDates(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Helper method to extract the substrings of a string which look like a date.
+        /// </summary>
+        /// <param name="text">The original string</param>
+        /// <returns>IEnumerable of the "looks like a date" substrings.</returns>
+        private IEnumerable<string> EnumerateDates(string text)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+# Szövegkezelés és I/O labor
+
+Ennek a labornak a célja a .NET szövegkezelésével, azon belül is elsősorban a reguláris kifejezés támogatással való megismerkedés.
+
+Feltételezzük, hogy korábbi tanulmányaitokból olyan metódusokat már ismertek, mint amivel meg lehet vizsgálni, hogy egy string azzal kezdődik-e, hogy "Hello". Ha mégsem, akkor itt a .NET eszköztára erre: [String methods](https://docs.microsoft.com/en-us/dotnet/api/system.string?view=netframework-4.7.2)
+
+A reguláris kifejezés egy olyan, bizonyos szintaktikai szabályok szerint leírt string, amivel meghatározható stringek egy halmaza. Segítségével könnyedén tudunk szövegrészleteket keresni vagy megvizsgálni, hogy egy adott szövegrészlet egy szabálynak megfelel-e. Például érvényes telefonszám vagy e-mail cím-e.
+
+# Felkészülés a laborra
+
+A felkészülés során az alábbi anyagokat tekintsd át:
+
+- Videók a Moodleben.
+- [Reguláris kifejezések C# segítségével](https://docs.microsoft.com/en-us/dotnet/api/system.text.regularexpressions.regex?view=netframework-4.7.2)
+- Tekintsd át a kiinduló forráskódot. A unit tesztekben szerepelnek kommentárok és tippek, valamint ezek ellenőrizik a megoldásaid helyes működését. A megoldások mind a Solutions.cs fájlba kerülnek majd.
+
+# A labor feladatok
+
+(A labor elején, már most hozz létre egy új branchet és utána azon dolgozz, hogy a pull requestet könnyű legyen majd a labor végén létrehozni! És ha az egyetemen kezdted el a munkát, mielőtt elmész, ne felejtsd el felpusholni a változásokat a laborgépről a githubra!)
+
+A labor feladat unit tesztek formájában adott. A projekt futtatható alkalmazást nem is tartalmaz, a cél a unit tesztek bezöldítése (csak a Solutions.cs fájlba dolgozz).
+
+A feladatokat a Solutions.cs fájlban szereplő sorrendben érdemes elkészíteni.
+Minden elkészítendő metódushoz vannak unit tesztek (lásd Test Explorer), amiben tippek is szerepelnek, hogy hogyan kell megoldani őket.
+A cél a unit tesztek bezöldítése (anélkül, hogy a unit teszteket módosítanád. :) ).
+
+Az egyes feladat csoportok az alábbiak (lásd unit teszt csoportok), a javasolt sorrendben:
+- EmailTests: e-mail címek felismerése
+- PhoneNumberTests: telefonszámok felismerése
+- MusicBoxTests: a "Mi van a zenedobozban?" játék, leírása a unit tesztek elején. Csoportos kitalálós játék. Akik még nem ismerik, azoknak példák alapján rá kell jönnie a szabályra, amivel eldől, hogy valami benne van-e a zenedobozban. Pl. "fűzfák" vannak benne, de "ékszer" nincs.
+- PlusCodeTests: A Google által kifejlesztett Plus Code (avagy Open Location Code) felismerése. https://en.wikipedia.org/wiki/Open_Location_Code
+- DateTests: pár dátum felismeréssel kapcsolatos feladat, hogy például ne lehessen május 35. (Kivéve itt: https://moly.hu/konyvek/erich-kastner-majus-35)
+
+A legtöbb feladatben először egy stringről kell eldönteni, hogy az-e, amit vizsgáltunk, utána pedig hasonlóan reguláris kifejezéssel egy szövegből kell kigyűjteni az ennek megfelelő részleteket, pl. telefonszámokat.
+
+Bág még később kerül elő, a Soltions.Collect metódus visszatérési értéke IEnumerable<string>. Erről most elég annyit tudni, hogy stringek egy felsorolása, amire ha hívunk egy .ToArray()-t, akkor string[] tömb lesz belőle. Ezt láthatjátok a Solutions.CollectingMatchesExample metódusban is.
